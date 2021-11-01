c627ca77a91674a7c6c63c47c645e4fcdda73d96
DukeChiu <duke00360753@gmail.com>
2021-11-01 18:50:57 +0800

    add iteration with tdd

---
+++ iteration/iteration_test.go
@@ -0,0 +1,26 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleRepeat() {
+	repeat := Repeat("b")
+	fmt.Println(repeat)
+	//Output: bbbbb
+}
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
