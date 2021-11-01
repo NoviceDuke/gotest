cf0cf15b611bfb5c4344c5b1182339a445f26565
DukeChiu <duke00360753@gmail.com>
2021-11-01 17:47:50 +0800

    add hello word with tdd refactor

---
+++ helloworld/hello.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+const englistHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const french = "French"
+const spanish = "Spanish"
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englistHelloPrefix
+	}
+	return
+}
+func main() {
+	fmt.Println(Hello("world", ""))
+}
