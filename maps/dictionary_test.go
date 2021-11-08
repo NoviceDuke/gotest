fad7a6fcc5f1b3605bab6d441d74216c8f4f8015
DukeChiu <duke00360753@gmail.com>
2021-11-08 19:46:03 +0800

    add map struct test

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -2,13 +2,63 @@ package main
 
 import "testing"
 
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+
+		dictionary := Dictionary{}
+		word := "test"
+		difinition := "this is just a test"
+		dictionary.Add(word, difinition)
+		assertDefinition(t, dictionary, word, difinition)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just test"
+
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	if definition != got {
+		t.Errorf("got %q want %q", got, definition)
+	}
+}
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
+	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
 
+		assertStrings(t, got, want)
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
+}
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
