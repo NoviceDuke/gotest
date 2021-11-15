26fe2815ca78dc21136d3822f1418c35855ac202
DukeChiu <duke00360753@gmail.com>
2021-11-15 15:02:07 +0800

    add concurrency test by using channel to solve race condition

---
+++ concurrency/concurrency_test.go
@@ -0,0 +1,45 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func mockWebsiteChecker(url string) bool {
+	return url != "want://further.gets"
+}
+
+func TestWebsiteChecker(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"want://further.gets",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"want://further.gets":        false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("Wanted %v, got %v", want, got)
+	}
+}
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = " a url"
+	}
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
